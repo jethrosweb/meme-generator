d5486ef0136e62b3d492f4740f518600ea08f6be
Jethro Dean <jethroldean@gmail.com>
2022-04-02 10:32:58 +1100

    Console log random memesData (native), next learning state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,22 @@
 import React from "react"
+import memesData from "../memesData"
 
 export default function Meme() {
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
     return (
         <main>
-            <form className="meme--form">
+            <div className="meme--form">
                 <input type="text" placeholder="Top text" className="meme--inputs" />
                 <input type="text" placeholder="Bottom text" className="meme--inputs" />
-                <button className="meme--button">Get a new meme image 🖼</button>
-            </form>
+                <button onClick={getMemeImage} className="meme--button">Get a new meme image 🖼</button>
+            </div>
         </main>
     )
 }
