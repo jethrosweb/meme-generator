4d1df3e8be33403b78adc4c33407c36f80819e51
Jethro Dean <jethroldean@gmail.com>
2022-04-02 13:05:48 +1100

    Generating meme images using state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,11 +3,12 @@ import memesData from "../memesData"
 
 export default function Meme() {
 
+    let [memeImage, setMemeImage] = React.useState("")
+
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        console.log(url)
+        setMemeImage(memesArray[randomNumber].url)
     }
 
     return (
@@ -17,19 +18,8 @@ export default function Meme() {
                 <input type="text" placeholder="Bottom text" className="meme--inputs" />
                 <button onClick={getMemeImage} className="meme--button">Get a new meme image 🖼</button>
             </div>
+            <img src={memeImage} className="meme--image" />
         </main>
     )
 }
 
-/* 
-Hanging state quiz:
-
-1. You have 2 options for what you can pass in to a state setter function (e.g. 'setCount'). What are they? 
-A: Callback function or new value of state. 
-
-2. When would you want to pass the first option (form answer above) to the state setter function? 
-A: When using the intial useState value to calculate the new state value. 
-
-3. When would you want to pass the second option (form answer above) to the state setter function? 
-A: When the intial useState value is not needed to calculate the new state value. 
-*/
