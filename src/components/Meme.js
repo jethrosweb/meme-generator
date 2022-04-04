f2460d241f1c7580909a2e65f1a9c78a6a4b9634
Jethro Dean <jethroldean@gmail.com>
2022-04-04 19:00:16 +1000

    Conditional rendering questions

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,12 +3,22 @@ import memesData from "../memesData"
 
 export default function Meme() {
 
-    let [memeImage, setMemeImage] = React.useState("")
+    let [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    let [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage() {
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
     }
 
     return (
@@ -18,8 +28,24 @@ export default function Meme() {
                 <input type="text" placeholder="Bottom text" className="meme--inputs" />
                 <button onClick={getMemeImage} className="meme--button">Get a new meme image 🖼</button>
             </div>
-            <img src={memeImage} className="meme--image" />
+            <img src={meme.randomImage} className="meme--image" />
         </main>
     )
 }
+
+/* Conditional rendering questions: 
+
+1. What is conditional rendering? 
+A: To determine certain outcomes depending on the given conditions. 
+
+2. When would you use &&? 
+A: When to display something or not display something (e.g. booleans).
+
+3. When would you use a ternary? 
+A: Which of two outcomes to display. 
+
+4. What if you need to decide between 2 options on what to display? 
+A: Use a regular conditional statement (if, if else, or switch).
+
+*/
 
