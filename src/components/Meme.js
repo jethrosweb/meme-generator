260a0ba31c336a1922e216fa98f1815b66469488
Jethro Dean <jethroldean@gmail.com>
2022-04-05 12:31:23 +1000

    Meme styling & form enabled

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,13 +3,23 @@ import memesData from "../memesData"
 
 export default function Meme() {
 
-    let [meme, setMeme] = React.useState({
+    const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
 
-    let [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage() {
         const memesArray = allMemeImages.data.memes
@@ -24,30 +34,29 @@ export default function Meme() {
     return (
         <main>
             <div className="meme--form">
-                <input type="text" placeholder="Top text" className="meme--inputs" />
-                <input type="text" placeholder="Bottom text" className="meme--inputs" />
+                <input 
+                    type="text" 
+                    placeholder="Top text" 
+                    className="meme--inputs" 
+                    name="topText"
+                    onChange={handleChange}
+                    value={meme.topText}
+                />
+                <input 
+                    type="text" 
+                    placeholder="Bottom text" 
+                    className="meme--inputs" 
+                    name="bottomText"
+                    onChange={handleChange}
+                    value={meme.bottomText}
+                />
                 <button onClick={getMemeImage} className="meme--button">Get a new meme image 🖼</button>
             </div>
-            <img src={meme.randomImage} className="meme--image" />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
-
-/* Form Questions: 
-
-1. In a vanilla JS app, at what point in the form submission process do you gather all the data from the filled-out form? 
-A: When the form is submitted. 
-
-2. In a React app, when do you gather all the data from the filled-out form?
-A: When the component state changes, meaning as soon as an elements value (or checked) is changed. Thus, as the form is being completed. 
-
-3. Which attribute in the form elements (value, name, onChange, etc.) should match the property name being held in state for that input? 
-A: Name.
-
-4. What's different about saving the data from a checkbox element vs. other form elements? 
-A: It's a boolean so the checked property is used to determine the input not the value property. 
-
-5. How do you watch for a form submit? How can you trigger a form submit? 
-A: Add the onSubmit handler on the form element - typically triggered using a <button> event listener. 
-
-*/
