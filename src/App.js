a6b0a8b6bba30c008b3ea1c62c276a300f8ecfc0
Jethro Dean <jethroldean@gmail.com>
2022-04-06 16:28:14 +1000

    Completed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Meme from "./components/Meme"
 
 export default function App() {
     return (
-        <div>
+        <div className="app--container">
             <Header />
             <Meme />
         </div>
